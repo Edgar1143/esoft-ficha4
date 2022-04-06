c06d0b0336536af3cd0d56a068f2c52d68ade3f2
João Santos <joaomeneses1804@gmail.com>
2022-04-06 10:45:38 +0100

    commit janelaprincipal

---
+++ src/main/java/JanelaPrincipal.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class JanelaPrincipal {
+    private JPanel panel1;
+    private JButton sairButton;
+    private JButton fazerUmRegistoButton;
+    private JButton visualizarOHistóricoButton;
+    private JTextField textField1;
+   // var janelaPrincipal = new JanelaPrincipal();
+
+    private void buttonSairActionPerformed(Action e){
+        System.exit(0);
+    }
+
+
+}
